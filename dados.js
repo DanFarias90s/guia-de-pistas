a2bef264616d4deeffe105b0708d78af23e5c073
DanFarias <dandanilodan90s@gmail.com>
2024-09-09 21:31:26 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,29 @@
+let dados = [
+    {
+      titulo: "Skate Park D'avó Hell",
+      descricao: "Pista clássica de São Miguel, localiza embaixo do viaduto da Jacu Pessego com diversas opções de obstáculos, ideal para todos os níveis.",
+      tipo: [{"Street", "Bowl"}], // Tipo como array para múltiplos valores
+      localizacao: "Vila Jacuí, São Paulo - SP, 08060-005",
+      avaliacao: "⭐⭐⭐⭐",
+      link: "https://www.google.com/maps/place/Skate+Park+D'av%C3%B3+Hell/@-23.4993691,-46.4571939,15z/data=!4m6!3m5!1s0x94ce616ce854cc03:0x8f7ee45373829651!8m2!3d-23.4993691!4d-46.4571939!16s%2Fg%11dxq906db?entry=ttu&g_ep_g=EgoyMDI4MDkwMi4xIKXMDSoASAFQAw%3D%D"
+      tags: "pista são miguel hell viaduto"
+    },
+    {
+      titulo: "Quadrinha Life Skateboard",
+      descricao: "Uma quadrinha com solo liso, repleta de obstaculos.",
+      tipo: [{"Street"}]
+      localizacao: "R. Jasmim de Porcelana - Jardim Santana, São Paulo - SP", // Aspas completas
+      avaliacao: "⭐⭐⭐⭐⭐",
+      link: "https://www.google.com/maps/place/Quadrinha+Life+Skateboard/@-23.5144838,-46.4555269,15z/data=!4m6!3m5!1s0x94ce6146263fb607:0x359140cb3cd07d47!8m2!3d-23.5144838!4d-46.4555269!16s%2Fg%2F11fzf7r9w8?entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D"
+      tags: "life quadrinha solo jacupessego"
+    },
+    {
+      titulo: "Pista do Parque Jacui, 'Pista do Pantas'.",
+      descricao: "Fica dentro do parque, uma otima pista para os diversos niveis de squeitist@s.",
+      tipo: [{"Street"}]
+      localizacao: "Rua Catléias, 680 - Sao Miguel, São Paulo - SP", // Aspas completas
+      avaliacao: "⭐⭐⭐⭐⭐",
+      link: "https://www.google.com/maps/place/Vila+Jacu%C3%AD+-+Unidade+de+Lazer+Engenheiro+Antonio+Arnaldo+de+Queiroz+e+Silva/@-23.4853274,-46.4542169,17z/data=!3m1!4b1!4m6!3m5!1s0x94ce61759075c8cd:0x3815f5f79eda2268!8m2!3d-23.4853274!4d-46.4542169!16s%2Fg%2F11b6zwg6bg?entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D"
+      tags: "jacui pantanal são miguel parque"
+    }
+  ];
